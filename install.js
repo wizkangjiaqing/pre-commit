3ff6c96663558919b7eb6d839468330d242599af
wizkangjiaqing <kjq@wiz.cn>
2021-02-01 19:23:48 +0800

    Update install.js

--- install.js
+++ install.js
@@ -7,7 +7,7 @@ var fs = require('fs')
   , path = require('path')
   , os = require('os')
   , hook = path.join(__dirname, 'hook')
-  , root = path.resolve(__dirname, '..', '..', '..', '..')
+  , root = path.resolve(__dirname, '..', '..', '..')
   , exists = fs.existsSync || path.existsSync;
 
 //
